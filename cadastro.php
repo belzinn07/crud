dd5ccb9881d7832bc5d69af1c34bdcd39844e4c2
belzinn07 <netobelmiro1@gmail.com>
2024-02-29 14:56:19 -0300

    v6

---
+++ cadastro.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+</head>
+<body>
+    <div class="box">
+        <form action="">
+            <fieldset>
+                <legend>Formulário de clientes</legend>
+                <br>
+                <div class="inputBox">
+                    <input type="text" name="nome" id="nome" class="">
+
+                </div>
+
+            </fieldset>
+        </form>
+    </div>
+</body>
+</html>
