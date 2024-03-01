05071782a1aa7050b44c70c54a757bd425f63fd0
belzinn07 <netobelmiro1@gmail.com>
2024-03-01 09:57:45 -0300

    v6

--- cadastro.php
+++ cadastro.php
@@ -12,9 +12,51 @@
                 <legend>Formulário de clientes</legend>
                 <br>
                 <div class="inputBox">
-                    <input type="text" name="nome" id="nome" class="">
-
+                    <input type="text" name="nome" id="nome" class="inputUser" required>
+                    <label for="nome"class="labelInput">Nome completo</label>
                 </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="nome" id="nome" class="inputUser" required>
+                    <label for="nome"class="labelInput">Nome completo</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="email" id="email" class="inputUser" required>
+                    <label for="email"class="labelInput">Email</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="tel" name="telefone" id="telefone" class="inputUser" required>
+                    <label for="telefone"class="labelInput">Telefone</label>
+                </div>
+                <br><br>
+                
+                    <input type="text" name="data_nascimento" id="data_nascimento" class="inputUser" required>
+                    <label for="data_nascimento"class="labelInput">Data de Nascimento</label>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="cidade" id="cidade" class="inputUser" required>
+                    <label for="cidade"class="labelInput">Cidade</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="estado" id="estado" class="inputUser" required>
+                    <label for="estado"class="labelInput">Estado</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="endereço" id="endereço" class="inputUser" required>
+                    <label for="endereço"class="labelInput">Endereço</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="password" name="senha" id="senha" class="inputUser" required>
+                    <label for="senha"class="labelInput">Senha</label>
+                </div>
+                <br><br>
+
+
 
             </fieldset>
         </form>
